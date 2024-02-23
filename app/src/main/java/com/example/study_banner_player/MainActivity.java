fa73549759e7a9a10f4fd2598d23bee8177c49df
ccutea <2204020224@qq.com>
2024-02-23 09:12:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/study_banner_player/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.study_banner_player;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleOwner;
+
+import android.app.PictureInPictureParams;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.youth.banner.Banner;
+import com.youth.banner.adapter.BannerAdapter;
+import com.youth.banner.adapter.BannerImageAdapter;
+import com.youth.banner.holder.BannerImageHolder;
+import com.youth.banner.indicator.CircleIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    //    private int[] img = {R.drawable.one, R.drawable.two, R.drawable.there, R.drawable.four};
+    private List<Drawable> img = new ArrayList<>();    private Banner banner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img.add(getDrawable(R.drawable.one));
+        img.add(getDrawable(R.drawable.two));
+        img.add(getDrawable(R.drawable.there));
+        img.add(getDrawable(R.drawable.four));
+        initBanner();
+    }
+
+    private void initBanner() {
+        banner = findViewById(R.id.id_banner);
+        banner.setAdapter(new BannerImageAdapter<Drawable>(img) {
+
+            @Override
+            public void onBindView(BannerImageHolder holder, Drawable data, int position, int size) {
+holder.imageView.setImageDrawable(img.get(position));
+            }
+        });
+    }}
